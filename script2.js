9e9228fce9e8a740f236c32decdc99f86abf21e5
gitmohitco <152581299+gitmohitco@users.noreply.github.com>
2024-01-27 10:43:46 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,24 @@
+const lmail = document.getElementById('mail')
+const lpass = document.getElementById('pass');
+document.getElementById('login').addEventListener('submit', login);
+function login(e){
+    let values = JSON.parse(localStorage.getItem("formData"))
+    
+    if((lmail.value == values.mail)&&(lpass.value == values.pass)){
+        window.location = "https://gitmohitco.github.io/formshow/";
+    }
+    else{
+        alert("Wrong email or password");
+    }
+    e.preventDefault();
+}
+document.getElementById('eye').addEventListener('click',function(){
+    if(lpass.type == "password"){
+        lpass.type = "text";
+        document.getElementById('eye').textContent = '😵';
+    }
+    else{
+        lpass.type = "password";
+        document.getElementById('eye').textContent = "🤨"
+    }
+})
