4275e4b35deb609c9ac7e44f94bbccf1a64f5cd9
gitmohitco <152581299+gitmohitco@users.noreply.github.com>
2024-01-30 16:36:56 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,16 +1,16 @@
-const lmail = document.getElementById('mail')
+const lmail = document.getElementById('mail');
 const lpass = document.getElementById('pass');
-document.getElementById('myForm').addEventListener('submit', login);
+document.getElementById('myForm').addEventListener('submit', login)
 function login(e){
+    e.preventDefault();
     let values = JSON.parse(localStorage.getItem("formData"))
-    
+    console.log(values)
     if((lmail.value == values.mail)&&(lpass.value == values.pass)){
         window.location.href = "https://gitmohitco.github.io/formshow/";
     }
     else{
         alert("Wrong email or password");
     }
-    e.preventDefault();
 }
 document.getElementById('eye').addEventListener('click',function(){
     if(lpass.type == "password"){
